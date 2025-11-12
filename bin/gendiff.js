f240779f4ca8166ec5d77cc90207ef55ad424f29
elvig <elvig.shakirov@gmail.com>
2025-11-12 13:03:31 +0300

    second commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,4 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
 
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
@@ -9,11 +9,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
   });
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   // Здесь будет реализация сравнения файлов
-  // Пока возвращаем заглушку
-  return `Difference between ${filepath1} and ${filepath2}`;
+  // Пока возвращаем заглушку для демонстрации
+  return `Comparing ${filepath1} and ${filepath2}${format ? ` with format: ${format}` : ''}`;
 };
 
 export default genDiff;
